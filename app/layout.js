2aed3d32a08a349dd426346b392d50cb3348cf7c
sagar sangwan <iamsagarsangwan@gmail.com>
2025-05-17 08:58:12 +0530

    added clarity script

--- app/layout.js
+++ app/layout.js
@@ -89,6 +89,17 @@ export default function RootLayout({ children }) {
           {children}
           <Toaster position="top-right" richColors />{" "}
         </body>
+        {process.env.NODE_ENV === "production" && (
+          <Script id="clarity_script_script" strategy="afterInteractive">
+            {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rks1x59zk1");
+          `}
+          </Script>
+        )}
       </Providers>
     </html>
   );
